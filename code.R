b2a411a97b0cb8fa4dd97ed80071f4f2ef2d6d16
Justo <justomatias@hotmail.com>
2020-12-27 03:25:08 -0300

    my first script

---
+++ code.R
@@ -0,0 +1,8 @@
+###############
+### CURSO 5 ###
+###############
+
+install.packages("tidyverse") #Con esta instrucción, se instala el paquete "tidyverse".
+install.packages(c("tidyverse", "dslabs")) #Aquí, se instalan dos paquetes (también, se aplica para 3 o más).
+
+installed.packages() #Se muestran todos los paquetes instalados en R.
